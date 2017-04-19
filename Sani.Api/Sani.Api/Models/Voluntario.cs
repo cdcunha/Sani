5520c1073d91497c73af4f5e27ae93b1406a4865
Charlestown Dias da Cunha <cdcunha@gmail.com>
2017-04-19 09:07:38 -0300

    Acrescentado novo campo

--- Sani.Api/Sani.Api/Models/Voluntario.cs
+++ Sani.Api/Sani.Api/Models/Voluntario.cs
@@ -6,7 +6,8 @@ namespace Sani.Api.Models
     [DataContract]
     public class Voluntario : BasePessoa
     {
-        
+        [DataMember]
+        public string Profissao { get; set; }
 
         [BsonConstructor]
         public Voluntario(string nome) : base(nome)
